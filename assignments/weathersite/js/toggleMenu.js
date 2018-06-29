71053d0d3d8a808de7181aac4d5bd33188b41a1e
kayjb <38710045+kayjb@users.noreply.github.com>
2018-06-29 14:46:24 -0600

    fixed hamburger button

--- assignments/weathersite/js/toggleMenu.js
+++ assignments/weathersite/js/toggleMenu.js
@@ -1,3 +1,9 @@
-function toggleMenu() {
-  document.getElementById("navigation").classList.toggle("hide");
+ function toggleHam() {
+    var x = document.getElementById("navigation");
+    if (x.className === "hide") {
+        x.className += " responsive";
+    } 
+    else {
+        x.className = "topnav";
+    }
 }
